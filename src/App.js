f3e17b948b29999c3ea3761cb0967e7c36ef7db2
Joan Menchise <jmenchise@gmail.com>
2023-05-12 20:08:41 -0300

    Maquetado de Cart.jsx

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
 import Details from './pages/Details';
+import Cart from './components/Cart';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      {/* <Home/> */}
       {/* <Details/> */}
+      <Cart/>
     </div>
   );
 }
